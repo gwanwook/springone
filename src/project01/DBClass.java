8e350193d1b516ac81455123a1e2a113e73a1619
jiyeon2445 <74689080+jiyeon2445@users.noreply.github.com>
2021-03-10 22:25:05 +0900

    기본프로젝트 수정

--- src/project01/DBClass.java
+++ src/project01/DBClass.java
@@ -1,21 +1,15 @@
 package project01;
 
-<<<<<<< HEAD
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 
-public class DBClass {
-	private String url = "jdbc:oracle:thin:@210.108.48.214:1521:xe";
-	private String id = "springone";
-	private String pwd = "1234";
-=======
+
 public class DBClass {
 	private String url = "jdbc:oracle:thin:@210.108.48.214:1521:xe";
 	private String id = "springone";
 	private int pwd = 1234;
->>>>>>> 09d316f09a18e57a751b2af7b9977f1e10dcce36
 	
 	public DBClass() {
 		try {
@@ -24,7 +18,7 @@ public class DBClass {
 			e.printStackTrace();
 		}
 	}
-<<<<<<< HEAD
+
 	public int modifyMember(Member m) {
 		String sql = "update newst1 set name=?, age=? where id=?";
 		int result = 0;
@@ -41,6 +35,6 @@ public class DBClass {
 		return result;	
 	}
 }
-=======
-}
->>>>>>> 09d316f09a18e57a751b2af7b9977f1e10dcce36
+
+
+
