5085b9cddc6ef08e141fa1def5a128bbbf7a8f3c
gwanwoo.kim <gwanwoo@aaa.com>
2021-03-11 14:02:04 +0900

    gwanwoo_branch 작업

--- src/project01/DBClass.java
+++ src/project01/DBClass.java
@@ -3,6 +3,7 @@ package project01;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 
 public class DBClass {
@@ -14,6 +15,24 @@ public class DBClass {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public void getMember() {
+		String sql = "select * from newst1";
+		try {
+			Connection con = DriverManager.getConnection(url,id,pwd);
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			System.out.println("아이디\t이름\t나이");
+			System.out.println("-----------------------");
+			while(rs.next()) {
+				System.out.println(rs.getString("id"));
+				System.out.println(rs.getString("name"));
+				System.out.println(rs.getInt("age"));
+				System.out.println("=================");
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -45,5 +64,18 @@ public class DBClass {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public int deleteMember(String id) {
+		String sql = "delete from newst1 where id=?";
+		int result = 0;
+		try {
+			Connection con = DriverManager.getConnection(url,this.id,pwd);
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, id);
+			result = ps.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
--- src/project01/MainClass.java
+++ src/project01/MainClass.java
@@ -16,21 +16,8 @@ public class MainClass {
 			System.out.print(">>> ");
 			num = input.nextInt();
 			switch(num) {
-			case 1: break;
-			case 3:
-				System.out.print("비교 아이디 입력 : ");
-				m.setId(input.next());
-				System.out.print("수정 이름 입력 : ");
-				m.setName(input.next());
-				System.out.print("수정 나이 입력 : ");
-				m.setAge(input.nextInt());
-				
-				int re = db.modifyMember(m);
-				if(re == 1) {
-					System.out.println("성공적으로 수정했습니다");
-				}else {
-					System.out.println("수정 중 문제가 발생했습니다");
-				}
+			case 1: 
+				db.getMember();
 				break;
 			case 2: 
 				System.out.print("아이디 입력: ");
@@ -41,7 +28,31 @@ public class MainClass {
 				m.setAge(input.nextInt());
 				db.saveMember(m);
 				break;
-			case 4: break;
+			case 3:
+				System.out.print("비교 아이디 입력 : ");
+				m.setId(input.next());
+				System.out.print("수정 이름 입력 : ");
+				m.setName(input.next());
+				System.out.print("수정 나이 입력 : ");
+				m.setAge(input.nextInt());
+
+				int re = db.modifyMember(m);
+				if(re == 1) {
+					System.out.println("성공적으로 수정했습니다");
+				}else {
+					System.out.println("수정 중 문제가 발생했습니다");
+				}
+				break;
+			case 4: 
+				System.out.print("삭제 아이디 입력 : ");
+				String id = input.next();
+				int result = db.deleteMember(id);
+				if(result == 1) {
+					System.out.println("성공적으로 삭제했습니다");
+				}else {
+					System.out.println("삭제중 문제가 발생했습니다");
+				}
+				break;
 			case 5: 
 				System.out.println("프로그램 종료합니다");
 				return;
