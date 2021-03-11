05504db2a2136b0a31f2b2d23f3ca703fb2a3231
gwanwoo.kim <gwanwoo@aaa.com>
2021-03-11 13:54:49 +0900

    project01 수정

---
+++ src/project01/DBClass.java
@@ -0,0 +1,49 @@
+package project01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+
+public class DBClass {
+	private String url = "jdbc:oracle:thin:@210.108.48.214:1521:xe";
+	private String id = "springone";
+	private String pwd = "1234";
+
+	public DBClass() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public int modifyMember(Member m) {
+		String sql = "update newst1 set name=?, age=? where id=?";
+		int result = 0;
+		try {
+			Connection con = DriverManager.getConnection(url,id,pwd);
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(3, m.getId());
+			ps.setString(1, m.getName());
+			ps.setInt(2, m.getAge());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;	
+	}
+
+	public void saveMember(Member m) {
+		String sql = "insert into newst1(id, name, age) values(?,?,?)";
+		try {
+			Connection con = DriverManager.getConnection(url, id, pwd);
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, m.getId());
+			ps.setString(2, m.getName());
+			ps.setInt(3, m.getAge());
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
